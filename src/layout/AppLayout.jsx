be57c5e2a740ff5e30794a61512c4a6b8de2b966
Abdul <rahman778@gmail.com>
2024-04-20 13:15:00 +0530

    basic component implementation

--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -1,8 +1,18 @@
 import { Outlet } from "react-router-dom";
+import ProductCard from "../components/cards/ProductCard";
+import CategoryTile from "../components/tiles/CategoryTile";
 
 function AppLayout() {
    return (
       <>
+         <div className="max-w-[300px] p-3 m-5">
+            <ProductCard />
+         </div>
+
+         <div className="max-w-[300px] p-3 m-5">
+            <CategoryTile title={"Men's Clothing"} />
+         </div>
+
          <main>
             <Outlet />
          </main>
